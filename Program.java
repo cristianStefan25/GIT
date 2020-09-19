46fb2e3ad41f64bfa7854e97de2c07ec6ca151da
Cristian Nastase <cnastase@iqnox.com>
2020-09-20 00:46:51 +0300

    Added another method (metoda1)

--- Program.java
+++ Program.java
@@ -1,5 +1,10 @@
 public class Program {
 	public static void main(String [] args) {
 		System.out.println("Hello from JAVA!!");
+		metoda1();
+	}
+
+	public static void metoda1() {
+		System.out.println("AM cea mai mare valoare!");
 	}
 }
